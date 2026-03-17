4508db9336661ef37b956bb4690af3c963d7445c
farouk24967 <bouayedfarouk63@gmail.com>
2026-03-17 16:38:06 +0100

    hgh

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting deployment to Vercel...
-echo If this is your first time, a browser window will open for you to log in.
-echo.
-call npx -y vercel --prod
-echo.
-echo Deployment command finished.
-pause
